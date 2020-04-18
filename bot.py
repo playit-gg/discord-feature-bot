7e1a320420946359b28697972b86a210a86d0153
Jozef Bonnar <42479938+Alienwarez123@users.noreply.github.com>
2020-04-18 18:24:25 +0100

    Added help and much more

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from datetime import date, datetime, timedelta
 from discord.ext import commands
 TOKEN = open("discord.key","r").readline()
 bot = commands.Bot(command_prefix='!')
-
-timeout = 4
+bot.remove_command("help")
+timeout = 2
 logs = {}
 
 @bot.event
@@ -26,18 +26,17 @@ async def on_message(message):
 async def on_message(message):
     if message.author == bot.user:
         return
-    if message.author.name in logs:
-        delta = message.created_at-logs[message.author.name].lastMessage
+    if message.author.id in logs:
+        delta = message.created_at-logs[message.author.id].lastMessage
         userid = message.author.id
         if(delta.seconds < timeout):
-            logs[message.author.name].violations += 1
+            logs[message.author.id].violations += 1
             await message.delete()
             await(await message.channel.send(f'<@!{userid}> no spamming allowed!'.format(message.author))).delete(delay=5)
             await asyncio.sleep(4)
-        
-        logs[message.author.name].lastMessage = message.created_at
+        logs[message.author.id].lastMessage = message.created_at
     else:
-        logs[message.author.name] = Log(message.created_at)
+        logs[message.author.id] = Log(message.created_at)
     await bot.process_commands(message)
 
 
@@ -49,26 +48,30 @@ async def ping(ctx):
 async def timeoutset(ctx, span : int):
     timeout = span
     await ctx.send('Updated spam timeout to {0} seconds.'.format(timeout))
-
+#member: Optional[discord.Member]=None IF I HAVE ANY FURTHER ARGUMENTS to help me in the future
 @bot.command(pass_context=True)
 async def checkspam(ctx, member : discord.Member= None):
-    name = ctx.author if not member else member
-    sub
-    print(name)
+    name = ctx.author.id if not member else member.id
     if name in logs:
         log = logs[name]
         if log.violations > 0:
-            await ctx.send('{} has {1.violations} violations.'.format(name, log))
+            await ctx.send(f'<@!{name}> ''has {1.violations} violations.'.format(name, log))
         else:
-            await ctx.send('{} has no violations yet.'.format(name))
+            await ctx.send(f'<@!{name}> ''has no violations yet.'.format(name))
     else:
-        await ctx.send('No user found or i am a rubbish dev')
+        await ctx.send('No user found or Alienwarez#0711 is a rubbish dev')
 
 
 @bot.command()
 @commands.is_owner()
 async def shutdown(ctx):
     await ctx.bot.logout()
+
+@bot.command()
+async def help(ctx):
+    help = open("help.txt","r")
+    await ctx.send(help.read())
+
 
 async def background_task():
     await bot.wait_until_ready()
